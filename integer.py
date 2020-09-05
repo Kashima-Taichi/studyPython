7bbf837130d1cfb6973e6d0aa93436958f4d1c33
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-09-05 18:16:02 +0900

    3-10　数値に関して

---
+++ integer.py
@@ -0,0 +1,32 @@
+import math
+
+print(2 + 2)
+print(5 - 2)
+print(5 * 6)
+print(50 - 5 * 6)
+print((50 - 5) * 6)
+print(8 / 5)
+print(type(1))
+print(type(1.6))
+print(.6)
+# 割り算
+print(17 / 3)
+# 商
+print(17 // 3)
+# 余
+print(17 % 3)
+# べき乗 2乗、5乗
+print(5 ** 2)
+print(5 ** 5)
+# 丸める
+pie = 3.1415151551515
+print(round(pie, 2))
+
+
+result = math.sqrt(25)
+print(result)
+
+y = math.log2(10)
+print(y)
+
+# print(help(math))
