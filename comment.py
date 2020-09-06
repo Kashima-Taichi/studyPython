260fb6b472dd8395bd4191f67c86918505b14ce6
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-09-06 23:10:36 +0900

    コメントに関して

--- comment.py
+++ comment.py
@@ -1,3 +1,7 @@
+# ###############
+# コメントアウトの方法
+# ###############
+
 print('XXXX')
 """
 複数行のコメントアウトはダブルクオートで囲む
@@ -10,3 +14,19 @@ print('XXXX')
 
 # apple price pythonの暗黙の文化ではコメントは変数宣言の上に書くらしい
 some_value = 100
+
+# ###############
+# 1行が長くなる場合
+# ###############
+
+# 80文字以上で改行がpythonのルールらしい
+
+# 丸括弧と+で改行する
+s = ('aaaaaaaaaaaaaaaaaaaa'
+     + 'aaaaaaaaaaaaaaaaaaaa')
+print(s)
+
+# バックスラッシュと+で改行する方法もある
+x = 'aaaaaaaaaaaaaaaaaaaa' \
+     + 'aaaaaaaaaaaaaaaaaaaa'
+print(x)
