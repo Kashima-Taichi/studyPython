579a2eee9f69138c1b1a2168112623cc46065226
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-09-09 23:12:57 +0900

    2020/09/09 パッケージのインポートとクラス

---
+++ lesson_package/talk/animal.py
@@ -0,0 +1,10 @@
+from lesson_package.tools import utils
+# from ..tools import utils
+
+
+def sing():
+    return '12342yrhuewffffndjksjoewfj9r'
+
+
+def cry():
+    return utils.say_twice('3829tgrfsfegnrmd')
