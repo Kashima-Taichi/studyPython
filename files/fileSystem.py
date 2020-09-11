3184a029eea85cb3243abd335b50238c00339d8f
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-09-11 23:28:28 +0900

    2020/09/11 基礎編終了

---
+++ files/fileSystem.py
@@ -0,0 +1,6 @@
+import os
+import pathlib
+import glob
+import shutil
+
+# ファイル操作について　8ふんあたりから
--- files/readFile.py
+++ files/readFile.py
@@ -5,15 +5,24 @@ CCC
 DDD
 """
 
-with open('test.txt', 'w') as f:
-    f.write(s)
+# with open('test.txt', 'w') as f:
+#     f.write(s)
 
 
 with open('test.txt', 'r') as f:
-    while True:
-        chunk = 2
-        line = f.read(chunk)
-        print(line)
-        if not line:
-            break
-
+    # while True:
+    #     chunk = 2
+    #     line = f.read(chunk)
+    #     print(line)
+    #     if not line:
+    #         break
+    print(f.tell())
+    print(f.read(1))
+    f.seek(5)
+    print(f.read(1))
+    f.seek(14)
+    print(f.read(1))
+    f.seek(15)
+    print(f.read(1))
+    f.seek(5)
+    print(f.read(1))
